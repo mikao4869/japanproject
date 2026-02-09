6192748801bf1a80859b1c4cbaf0e5799dfd6dca
박서현 <s24005@gsm.hs.kr>
2026-02-09 22:03:26 +0900

    vercel 배포-3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,13 @@
-"use client"
-
 import "../i18n/i18n";
-import { useTranslation } from "react-i18next";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const { t } = useTranslation();
-
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="ko">
       <body>
-        <header className="p-4 bg-gray-100">
-        
-        </header>
         <main>{children}</main>
       </body>
     </html>
